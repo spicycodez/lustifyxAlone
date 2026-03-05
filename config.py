1d307b0d9774102bd43a0db4085fd1eeba1d96f6
˹ ꜱᴘɪᴄʏ ᴄᴏᴅᴇʀꜱ ˼ <im_maxim@inbox.ru>
2026-03-05 13:48:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ LOGGER_ID = int(getenv("LOGGER_ID", 0))
 
 DEBUG_IGNORE_LOG = True
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 8106551502))
+OWNER_ID = int(getenv("OWNER_ID", 7603581459))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -44,15 +44,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
+    "https://github.com/spicycodez/lustifyxAlone",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheAloneTeam")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheTeamAlone")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SpIcYxNeTwOrK")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+sTyS-zKwUIk4YWI1")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", None))
