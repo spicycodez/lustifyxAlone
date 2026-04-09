2545d8ef195131be852375d3e9306a433eee6f9c
spicycodez <169600727+spicycodez@users.noreply.github.com>
2026-04-09 12:48:05 +0000

    𝚻հҽ 𝚨Łꪮⲛ𝛆 🚩𝗧ε᧘‌ᴍ + #⟶̽ जय श्री ༢།म >𝟑🙏🚩

--- AloneMusic/utils/inline/play.py
+++ AloneMusic/utils/inline/play.py
@@ -6,7 +6,7 @@
 # Please see < https://github.com/TheAloneTeam/AloneMusic/blob/master/LICENSE >
 #
 # All rights reserved.
- 
+
 import math
 
 from pyrogram.types import InlineKeyboardButton
--- AloneMusic/utils/thumbnails.py
+++ AloneMusic/utils/thumbnails.py
@@ -146,7 +146,9 @@ async def get_thumb(videoid):
             stroke_fill="black",
         )
 
-        stats_text = f"YouTube : {views} | Time : {duration} | Player : @LustifyMusicBoT"
+        stats_text = (
+            f"YouTube : {views} | Time : {duration} | Player : @LustifyMusicBoT"
+        )
         w_stats = get_text_width(stats_text, font_details)
         draw.text(
             ((1280 - w_stats) / 2, text_y_pos + 70),
